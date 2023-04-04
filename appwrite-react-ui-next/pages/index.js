2e0028190e0867283ce7503f324801b81b3288b0
faisalill <mfaisal.ilyas.d@gmail.com>
2023-04-04 21:15:47 +0530

    added refresh button

--- appwrite-react-ui-next/pages/index.js
+++ appwrite-react-ui-next/pages/index.js
@@ -12,7 +12,9 @@ const App = () => {
   const account = new Account(client)
   const [user, setUser] = useState({name: "Not Logged in", email: "Not Logged in", emailVerification: false, phone: "Not Logged In ", phoneVerification: false})
   const [loading, setLoading] = useState(true)
+  const [Refresh, setRefresh] = useState(false)
   useEffect(() => {
+    console.log('refresh')
     const promise = account.get();
     promise.then(function (response) {
       setUser(response)
@@ -20,7 +22,7 @@ const App = () => {
   }, function (error) {
       setLoading(false)
   });
-  }, [])
+  }, [Refresh])
   return (
     <>
     
@@ -36,6 +38,7 @@ const App = () => {
     {user && <p>Phone: &nbsp;<span className='info'>{user.phone}</span></p>}
     {user && <p>Email Verified: &nbsp; <span className='info'>{user.emailVerification ? 'yes' : 'no'}</span></p>}
     {user && <p>Phone Verified: &nbsp;<span className='info'>{user.phoneVerification ? 'yes': 'no'}</span></p>}
+    <div >
     <button
     className='sign-out-btn'
     onClick={()=>{
@@ -44,6 +47,14 @@ const App = () => {
       })
     }}
     >Sign Out</button>
+     <button
+    className='sign-out-btn'
+    onClick={()=>{
+      setRefresh(!Refresh)
+      setLoading(true)
+    }}
+    >Refresh</button>
+    </div>
      </div>  
   }
     <GoogleAuth client={client} successUrl={successUrl} failureUrl={failureUrl}/>
